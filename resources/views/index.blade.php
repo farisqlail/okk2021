57be053eb7b394ad4b6b68880b3641581a011eb8
farisqlail <faris.riskilail@gmail.com>
2021-08-16 08:34:29 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,469 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>OKK DINAMIKA 2021</title>
+  <meta content="" name="description">
+
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="<?php echo get_bloginfo( 'template_directory' );?>/assets/img/logo.png" rel="icon">
+  <link href="<?php echo get_bloginfo( 'template_directory' );?>/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@500;600&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+	<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+    integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+    integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+
+  <!-- Template Main CSS File -->
+  <link href="<?php echo get_bloginfo( 'template_directory' );?>/assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="hero d-flex align-items-center">
+
+    <div class="container">
+      <h1 align="center" data-aos="fade-up" data-duration="1000">OKK 2021</h1><br>
+      <h1 align="center" class="text-white" data-aos="fade-up" data-duration="2000">Universitas Dinamika</h1>
+      <p class="mt-3" data-aos="fade-up" data-duration="2500" align="center" style="color: #A6CAED;">Selamat datang mahasiswa baru 2021, we are team luar biasa,
+        semangat, berprestasi</p>
+
+      <div class="d-flex justify-content-center" data-aos="fade-up" data-duration="3000">
+        <!-- <a href="#about" align="center"><i class="fas fa-chevron-down" style="color: #fff;"></i></a> -->
+        <a href="#" class="btn button-download">Download File &nbsp; <i class="fas fa-arrow-down"></i></a>
+      </div>
+    </div>
+
+  </section><!-- End Hero -->
+
+  <main id="main">
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+
+      <div class="container" data-aos="fade-up">
+        <h1 align="center">Selamat Datang di OKK 2021!</h1>
+        <p class="mt-3" align="center" style="color: #A6CAED;">Apa itu Orientasi Kehidupan dan Kampus (OKK) 2021
+          Universitas Dinamika ? <br><br>
+          Sebuah kegiatan yang dilaksanakan oleh Universitas Dinamika guna untuk membimbing para
+          calon mahasiswa baru untuk menjadi mahasiswa yang siap untuk mengarungi kehidupan di Kampus Universitas
+          Dinamika.
+        </p>
+
+        <div class="row pt-lg-5">
+          <div class="col-md-4" align="center">
+            <img src="<?php echo get_bloginfo( 'template_directory' );?>/assets/img/seminar.png" class="img-fluid" width="182px" alt="">
+            <h3>Seminar Online</h3>
+            <p align="center" style="color: #A6CAED;">
+              Cara untuk memperluas wawasan
+              tentang kampus Universitas
+              Dinamika adalah melalui Seminar
+              yang akan disampaikan per bagian.
+            </p>
+          </div>
+          <div class="col-md-4" align="center">
+            <img src="<?php echo get_bloginfo( 'template_directory' );?>/assets/img/workshop.png" class="img-fluid" width="182px" alt="">
+            <h3>Workshop</h3>
+            <p align="center" style="color: #A6CAED;">
+              Pada rangkaian OKK 2020 terdapat
+              acara workshop GAFE dan Ms. Word
+              untuk meningkatkan produktivitas
+              mahasiswa saat menjalani perkuliahan
+              secara online.
+            </p>
+          </div>
+          <div class="col-md-4" align="center">
+            <img src="<?php echo get_bloginfo( 'template_directory' );?>/assets/img/kreatif.png" class="img-fluid" width="182px" alt="">
+            <h3>Ajang Kreativitas</h3>
+            <p align="center" style="color: #A6CAED;">
+              Ajang Kreativitas merupakan salah
+              satu cara untuk memperkuat
+              interaksi antara satu peserta
+              OKK dengan peserta OKK yang lain.
+              Hal ini dilakukan untuk memperkuat
+              sinergisitas antar mahasiswa.
+            </p>
+          </div>
+        </div>
+
+      </div>
+
+    </section><!-- End About Section -->
+
+    <!-- ======= tour Section ======= -->
+    <section id="tour" class="tour">
+
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6" data-aos="fade-right">
+            <h2>Tour de Dinamika</h2>
+            <p class="mt-3" style="color: #4F5551;">
+              Yuk kunjungi kampus Universitas Dinamika secara online!
+              Kamu bisa melihat sekeliling kampus juga karena kita tour
+              dengan menggunakan 360 camera.
+            </p>
+
+            <a href="360.php" class="btn button-tour">Mulai Tour &nbsp; <i
+                class="fas fa-arrow-right"></i></a>
+          </div>
+          <div class="col-md-6" data-aos="zoom-in">
+            <img src="<?php echo get_bloginfo( 'template_directory' );?>/assets/img/Saly-1.png" class="img-fluid saly" alt="">
+          </div>
+        </div>
+      </div>
+
+    </section><!-- End tour Section -->
+
+    <!-- Class Time Section Begin -->
+    <section class="classtime-section class-time-table spad">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <div class="section-title" data-aos="fade-up">
+              <h2 style="font-family: 'Roboto Slab', serif; font-weight: 500; font-size: 48px;">Jadwal Kegiatan</h2>
+              <p style="font-family: 'Roboto Slab', serif; font-size: 18px; color: #A6CAED;">Cek jadwal kegiatannya disini!</p>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="timetable-controls">
+            </div>
+          </div>
+        </div>
+        <div class="classtime-table" data-aos="fade-up">
+          <table>
+            <thead>
+              <tr>
+                <th>16 Agt 2021</th>
+                <th>17 Agt 2021</th>
+                <th>18 Agt 2021</th>
+                <th>19 Agt 2021</th>
+                <th>20 Agt 2021</th>
+                <th>21 Agt 2021</th>
+                <th>23 Agt 2021</th>
+                <th>24 Agt 2021</th>
+                <th>25 Agt 2021</th>
+                <th>26 Agt 2021</th>
+                <th>27 Agt 2021</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <span>08.45 - 11.00</span>
+                  <h6>Technical Meeting</h6>
+                </td>
+                <td class="hover-bg ts-item" data-tsmeta="praokk">
+              
+                </td>
+                <td class="hover-bg ts-item" data-tsmeta="okk1">
+                
+                </td>
+                <td>
+                  
+                </td>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+              </tr>
+              <tr>
+                <td>
+
+                </td>
+                <td class="hover-bg ts-item" data-tsmeta="praokk">
+                  <span>07.15 - 09.30</span>
+                  <h6>Upacara 17 Agustus</h6>
+                </td>
+                <td>
+
+                </td>
+                
+                <td class="hover-bg ts-item" data-tsmeta="okk2">
+                  <span>07.15 - 10.45</span>
+                  <h6>Day-2 PraOKK</h6>
+                </td>
+                <td class="hover-bg ts-item" data-tsmeta="okk3">
+                  
+                </td>
+                <td class="hover-bg ts-item" data-tsmeta="okk4">
+             
+                </td>
+                <td class="hover-bg ts-item" data-tsmeta="okk5">
+                 
+                </td>
+                <td class="hover-bg ts-item" data-tsmeta="okk1">
+                  <span>07.15 - 11.00</span>
+                  <h6>Day-2 OKK</h6>
+                </td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+
+              <tr>
+                <td class="hover-bg ts-item" data-tsmeta="tmokk">
+                  
+                </td>
+                <td class="hover-bg ts-item" data-tsmeta="praokk">
+                  
+                </td>
+                <td class="hover-bg ts-item" data-tsmeta="okk1">
+                  <span>07.15 - 10.45</span>
+                  <h6>Day-1 PraOKK</h6>
+                </td>
+                <td class="hover-bg ts-item" data-tsmeta="okk2">
+              
+                </td>
+                <td class="hover-bg ts-item" data-tsmeta="okk3">
+                 
+                </td>
+                <td class="hover-bg ts-item" data-tsmeta="okk4">
+                  
+                </td>
+                <td class="hover-bg ts-item" data-tsmeta="okk5">
+                  <span>07.15 - 11.15</span>
+                  <h6>Day-1 OKK</h6>
+                </td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+
+              <tr>
+                <td class="hover-bg ts-item" data-tsmeta="tmokk">
+                 
+                </td>
+                <td class="hover-bg ts-item" data-tsmeta="praokk">
+                  
+                </td>
+                <td class="hover-bg ts-item" data-tsmeta="okk1">
+                  
+                </td>
+                <td class="hover-bg ts-item" data-tsmeta="okk2">
+                  
+                </td>
+                <td class="hover-bg ts-item" data-tsmeta="okk3">
+                  <span>07.15 - 10.45</span>
+                  <h6>Day-3 PraOKK</h6>
+                </td>
+                <td class="hover-bg ts-item" data-tsmeta="okk4">
+                  
+                </td>
+                <td class="hover-bg ts-item" data-tsmeta="okk5">
+                 
+                </td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td class="hover-bg ts-item" data-tsmeta="okk1">
+                  <span>07.15 - 09.50</span>
+                  <h6>Day-5 OKK</h6>
+                </td>
+              </tr>
+
+              <tr>
+                <td class="hover-bg ts-item" data-tsmeta="tmokk">
+                  
+                </td>
+                <td class="hover-bg ts-item" data-tsmeta="praokk">
+              
+                </td>
+                <td class="hover-bg ts-item" data-tsmeta="okk1">
+                 
+                </td>
+                <td class="hover-bg ts-item" data-tsmeta="okk2">
+                
+                </td>
+                <td>
+
+                </td>
+                <td class="hover-bg ts-item" data-tsmeta="okk4">
+                  <span>07.00 - 12.00</span>
+                  <h6>Day-4 Pra</h6>
+                </td>
+                <td class="hover-bg ts-item" data-tsmeta="okk5">
+                  
+                </td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+
+              <tr>
+                <td class="hover-bg ts-item" data-tsmeta="tmokk">
+               
+                </td>
+                <td class="hover-bg ts-item" data-tsmeta="praokk">
+                  
+                </td>
+                <td class="hover-bg ts-item" data-tsmeta="okk1">
+               
+                </td>
+                <td class="hover-bg ts-item" data-tsmeta="okk2">
+                  
+                </td>
+                <td>
+
+                </td>
+                <td class="hover-bg ts-item" data-tsmeta="okk4">
+               
+                </td>
+                <td class="hover-bg ts-item" data-tsmeta="okk5">
+                  
+                </td>
+                <td></td>
+                <td class="hover-bg ts-item" data-tsmeta="okk1">
+                  <span>07.15 - 11.15</span>
+                  <h6>Day-3 OKK</h6>
+                </td>
+                <td></td>
+                <td></td>
+              </tr>
+
+              <tr>
+                <td class="hover-bg ts-item" data-tsmeta="tmokk">
+                  
+                </td>
+                <td class="hover-bg ts-item" data-tsmeta="praokk">
+         
+                </td>
+                <td class="hover-bg ts-item" data-tsmeta="okk1">
+                  
+                </td>
+                <td class="hover-bg ts-item" data-tsmeta="okk2">
+                  
+                </td>
+                <td class="hover-bg ts-item" data-tsmeta="okk3">
+                  
+                </td>
+                <td class="hover-bg ts-item" data-tsmeta="okk4">
+                  
+                </td>
+                <td class="hover-bg ts-item" data-tsmeta="okk5">
+               
+                </td>
+                <td></td>
+                <td></td>
+                <td class="hover-bg ts-item" data-tsmeta="okk1">
+                  <span>07.15 - 11.00</span>
+                  <h6>Day-4 OKK</h6>
+                </td>
+                <td></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </section>
+    <!-- Class Time Section End -->
+
+  </main><!-- End #main -->
+
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer" class="footer">
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row gy-4">
+          <div class="col-lg-5 col-md-12 footer-info">
+            <a href="index.html" class="logo d-flex align-items-center">
+              <img src="<?php echo get_bloginfo( 'template_directory' );?>/assets/img/logo-nav.png" alt="">
+              
+            </a>
+            <p style="color: #4F5551;">OKK Universitas Dinamika adalah kegiatan Orientasi Kehidupan dan Kampus yang mengenalkan kehidupan seorang mahasiswa baik di dalam lingkungan kampus Universitas Dinamika maupun di masyarakat.</p>
+            <div class="social-links mt-3">
+              <a href="https://www.instagram.com/okkdinamika/?hl=en" class="instagram"><i class="bi bi-instagram bx bxl-instagram"></i></a>
+            </div>
+          </div>
+
+          <div class="col-lg-2 col-6 footer-links">
+            <h4>Downloads</h4>
+            <ul>
+              <li><a href="#">Buku Panduan OKK 2021</a></li>
+              <li><a href="#">Twibbon OKK 2021</a></li>
+              <li><a href="#">Buku Panduan Pergantian Foto Profil & Nama Gmail</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-2 col-6 footer-links">
+           
+          </div>
+
+          <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
+            <h4>Kontak Kami</h4>
+            <p style="color: #4F5551;">
+              Jl. Raya Kedung Baruk 98, Surabaya <br><br>
+              <strong>Phone:</strong> +1 5589 55488 55<br>
+              <strong>Email:</strong> okk@dinamika.ac.id<br>
+            </p>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>OKK DINAMIKA 2021</span></strong>. All Rights Reserved
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+      class="fas fa-arrow-up"></i></a>
+
+  <!-- Vendor JS Files -->
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
+
+	<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+	
+	<!--Fixed AOS Not Showing Content-->
+	<script>AOS.init();</script>
+	<!--End of Script-->
+
+  <!-- Template Main JS File -->
+  <script src="<?php echo get_bloginfo( 'template_directory' );?>/assets/js/main.js"></script>
+
+</body>
+
+</html>
